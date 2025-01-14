768a35cc2f4bd8059fe9021a0ba50867f0c7b153
coolmp08 <coolmp08@gmail.com>
2025-01-14 16:36:52 +0530

    initial-commit

---
+++ src/main/java/io/devsense/graphQLdemo/GraphQLdemoApplication.java
@@ -0,0 +1,13 @@
+package io.devsense.graphQLdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQLdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQLdemoApplication.class, args);
+    }
+
+}
